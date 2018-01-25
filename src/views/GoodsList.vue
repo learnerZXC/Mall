462ef1e251197f8cb00154be188a6ad17aaa081f
learnerZXC <704253573@qq.com>
2018-01-25 23:14:18 +0800

    Vue商城
    实现图片价格区间和图片懒加载

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -10,25 +10,16 @@
           <span class="sortby">Sort by:</span>
           <a href="javascript:void(0)" class="default cur">Default</a>
           <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
-          <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+          <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
         </div>
         <div class="accessory-result">
           <!-- filter -->
-          <div class="filter stopPop" id="filter">
+          <div class="filter stopPop" id="filter" v-bind:class="{'filterby-show':filterBy}">
             <dl class="filter-price">
               <dt>Price:</dt>
-              <dd><a href="javascript:void(0)">All</a></dd>
-              <dd>
-                <a href="javascript:void(0)">0 - 100</a>
-              </dd>
-              <dd>
-                <a href="javascript:void(0)">100 - 500</a>
-              </dd>
-              <dd>
-                <a href="javascript:void(0)">500 - 1000</a>
-              </dd>
-              <dd>
-                <a href="javascript:void(0)">1000 - 2000</a>
+              <dd><a href="javascript:void(0)" v-bind:class="{'cur': priceChecked=='all'}"  v-on:click="priceChecked=all">All</a></dd>
+              <dd v-for="(price,index) in priceFilter" >
+                <a href="javascript:void(0)" v-on:click="setPriceFilter(index)" v-bind:class="{'cur':priceChecked==index}">{{ price.startPrice }} - {{ price.endPrice }} </a>
               </dd>
             </dl>
           </div>
@@ -39,7 +30,7 @@
               <ul>
                 <li v-for="(item,index) in goodsList ">
                   <div class="pic">
-                    <a href="#"><img v-bind:src="'/static/'+ item.productImg" alt=""></a>
+                    <a href="#"><img v-lazy="'/static/'+ item.productImg" alt=""></a>
                   </div>
                   <div class="main">
                     <div class="name">{{ item.productName }}</div>
@@ -55,6 +46,7 @@
         </div>
       </div>
     </div>
+    <div class="md-overlay" v-show="overLayFlag" @click='closePop'></div>
     <nav-footer></nav-footer>
 	</div>
 </template>
@@ -69,7 +61,24 @@
 	 export default{
         data(){
             return {
-              goodsList: []
+              goodsList: [],
+              priceFilter:[
+                {
+                  startPrice: '0.00',
+                  endPrice: '500.00'
+                },
+                {
+                  startPrice: '500.00',
+                  endPrice: '1000.00'
+                },
+                {
+                  startPrice: '1000.00',
+                  endPrice: '2000.00'
+                }
+              ],
+              priceChecked:'all',
+              filterBy:false,
+              overLayFlag:false
             }
         },
         components:{
@@ -86,7 +95,20 @@
               var res = result.data;
              this.goodsList = res.data.result;
             })
+          },
+          showFilterPop(){
+            this.filterBy=true;
+            this.overLayFlag=true;
+          },
+          setPriceFilter(index){
+            this.priceChecked = index;
+            this.closePop();
+          },
+          closePop(){
+            this.filterBy = false;
+            this.overLayFlag = false;
           }
+          
         }
     }
 </script>
